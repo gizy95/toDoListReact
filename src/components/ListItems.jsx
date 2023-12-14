468424fcd2c7838b5f7dd35a48923909d97c073f
Subaru Goto <subaru.dayo@gmail.com>
2023-12-14 18:04:55 +0100

    adding edit function

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -1,31 +1,20 @@
-import { storeTodoList, getRandomId } from "../../utils";
-
-
-export default function ListItems({ toDoList, setToDoList }) {
-
+export default function ListItems({ toDoList, setToDoList, getUpdateItem}) {
+  
   const deleteItem = (id) => {
     const newList = toDoList.filter(item => item.id !== id);
     setToDoList(newList);
-    localStorage.setItem('toDoList', JSON.stringify(newList));
   }
 
-
-
-
-
   return (
-
     <ul>
       {toDoList.map((item) => (
         <li id={item.id}>
           <span><input type="checkbox" id="checkbox" />
-            <span>{item.title}</span></span>
+            <span onClick={() => getUpdateItem(item.id)}>{item.title}</span></span>
           <button onClick={() => deleteItem(item.id)} className='delete-button'>X</button>
         </li>
       ))
       }
     </ul >
-
-
   )
 }
