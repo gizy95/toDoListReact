1fd1bc7a5004c817857039f0b6823309bf226434
Antonio Bjelobrk <bjelobrka@gmail.com>
2023-12-14 16:25:42 +0100

    added function to check items and make them strike out

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -6,10 +6,14 @@ export default function ListItems({ toDoList, setToDoList }) {
   const deleteItem = (id) => {
     const newList = toDoList.filter(item => item.id !== id);
     setToDoList(newList);
-    localStorage.setItem('toDoList', JSON.stringify(newList));
+    localStorage.setItem('todo', JSON.stringify(newList));
   }
 
-
+  const toggleChecked = (id) => {
+    const newList = toDoList.map(item => item.id === id ? { ...item, checked: !item.checked } : item);
+    setToDoList(newList);
+    localStorage.setItem('todo', JSON.stringify(newList));
+  }
 
 
 
@@ -18,8 +22,10 @@ export default function ListItems({ toDoList, setToDoList }) {
     <ul>
       {toDoList.map((item) => (
         <li id={item.id}>
-          <span><input type="checkbox" id="checkbox" />
-            <span>{item.title}</span></span>
+          <span>
+            <input type="checkbox" id="checkbox" checked={item.checked} onChange={() => toggleChecked(item.id)} />
+            <span className={item.checked ? 'strikethrough' : ''}>{item.title}</span>
+          </span>
           <button onClick={() => deleteItem(item.id)} className='delete-button'>X</button>
         </li>
       ))
